5cbda6275becd980fd6a6a605037f91a850d1d20
Dev-gojoy <32jongwoo@gmail.com>
2023-03-28 17:05:40 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello, React! </h1>
+      <h2>My name is Ko Jong Woo </h2>
+      <h3>It's too hard</h3>
+    </div>
+  );
+}
+
+export default App;
